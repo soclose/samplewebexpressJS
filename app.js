849bbe2159b45992fcccf2748b6473fbda29ed5e
root <root@informatika-NEON-HNM-MODEL.(none)>
2014-01-22 11:52:49 -0700

    version 1.1.1

---
+++ app.js
@@ -0,0 +1,32 @@
+/**
+ * Module for create server
+ */
+
+var express = require('express'),
+  http = require('http'),
+  path = require('path');
+
+var app = express();
+
+
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Home'
+  });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
